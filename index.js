92655ccc5a4bf306b069764308d95a8211d16620
Christina <cligare@hotmail.com>
2023-08-20 17:13:09 -0700

    complete project

--- index.js
+++ index.js
@@ -1,24 +1,55 @@
-// https://www.thecolorapi.com/scheme?
+const headerContainer = document.getElementById("header-container");
+const schemeContainer = document.getElementById("scheme-container");
+const toggle = document.getElementById("toggle");
 
-document.getElementById("select-btn").addEventListener("click", function () {
+document
+  .getElementById("select-btn")
+  .addEventListener("click", fetchAndRenderColorScheme);
+
+function fetchAndRenderColorScheme() {
   const seedColor = document.getElementById("seed-color").value.substring(1);
-  const mode = document.getElementById("color-scheme").value.toLowerCase();
-
+  const mode = document
+    .getElementById("color-scheme-selector")
+    .value.toLowerCase();
   fetch(`https://www.thecolorapi.com/scheme?hex=${seedColor}&mode=${mode}`)
     .then((response) => response.json())
     .then((data) => {
       // console.log(data.mode);
       let html = "";
       data.colors.forEach(function (color) {
-        console.log(color.hex.value);
         html += `
         <div class="color-column">
-        <div class="color" style="background-color:${color.hex.value}"></div>
-        <p class="hex">${color.hex.value}</p>
-      </div>
+          <div id=${color.hex.value} class="hex-color" data-tooltip="Copied!" style="background-color:${color.hex.value}">${color.name.value}</div>
+          <p id=${color.hex.value} class="hex-code hover-effect" data-tooltip="Copied!">${color.hex.value}</p>
+        </div>
         `;
       });
 
-      document.getElementById("scheme-container").innerHTML = html;
+      schemeContainer.innerHTML = html;
     });
+
+  schemeContainer.addEventListener("click", function (e) {
+    let copiedHexCode = e.target.id;
+    if (copiedHexCode) {
+      navigator.clipboard.writeText(copiedHexCode).then(() => {
+        tippy(e.target, {
+          content: "Copied!",
+          placement: "bottom",
+          theme: "tooltip",
+        });
+        e.target._tippy.show();
+      });
+    }
+  });
+}
+fetchAndRenderColorScheme();
+
+toggle.addEventListener("input", function (e) {
+  if (e.target.checked) {
+    headerContainer.classList.add("dark-mode");
+    schemeContainer.classList.add("dark-mode");
+  } else {
+    headerContainer.classList.remove("dark-mode");
+    schemeContainer.classList.remove("dark-mode");
+  }
 });
--- readme.md
+++ readme.md
@@ -19,8 +19,22 @@
 - Finalize README file
 - Publish live URL
 
-### Useful code snippets and tips --
+### Challenges along the way
+
+- Have problems making the options dropdown box dark
 
 ### Resources:
 
 [How to create a dropdown list](https://www.freecodecamp.org/news/html-drop-down-menu-how-to-add-a-drop-down-list-with-the-select-element/)
+
+[Copy text to clipboard](https://www.w3schools.com/howto/howto_js_copy_clipboard.asp)
+
+[Light/Dark Mode toggle](https://codepen.io/sashatran/pen/rPaLgG)
+
+[How to create a dark/light theme toggle](https://www.florin-pop.com/blog/2019/05/dark-light-theme-toggle/)
+
+[How to make tooltips with only CSS](https://www.youtube.com/watch?v=ujlpzTyJp-M)
+
+[tippy.js](https://atomiks.github.io/tippyjs/)
+
+[sweet alert](<(https://sweetalert2.github.io/)>)
